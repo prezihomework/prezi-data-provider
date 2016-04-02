a49e3db0d63ed01ba8731cda0fa3e5c11f8a2364
Gabor Benedek <gabor.benedek@sap.com>
2016-04-02 15:07:37 +0200

    Implement sort by date functionality

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ var restify = require("restify"),
 var PREZIS_DIR 	= "../data/";
 var prezis = new Array();
 
+//--------------------------------------------
+//  INITIALIZATION 
+//--------------------------------------------
+
 function readPrezis(){
 	console.log("Reading prezis");
 	fs.readdir(PREZIS_DIR, function(error, files){
@@ -22,7 +26,11 @@ function readPrezis(){
   });
 }
 
-readPrezis();
+function initialize(){
+  readPrezis();
+};
+
+initialize();
 
 var	server = restify.createServer({
     name: "PreziDataProvider",
@@ -35,6 +43,10 @@ server.listen(8080, function() {
 
 server.use(restify.queryParser());
 
+//--------------------------------------------
+//  COMMON API 
+//--------------------------------------------
+
 server.pre(function(req, res, next) {
 	console.log("REQUEST: "+req.method +" "+req.url);
   	return next();
@@ -44,8 +56,59 @@ server.on('after', function (request, response, route, error) {
 	console.log("RESPONSE: "+response.statusCode);
 });
 
+//--------------------------------------------
+//  PREZIS APIs
+//--------------------------------------------
+
+// return list of prezis
+// can be filtered by title parameter 
 server.get("/prezis",function(req, res, next){
   res.contentType = "application/json";
-  res.send(prezis);
+
+  if(req.params.title === undefined){
+    res.send(prezis);
+  } else {
+    result = findPreziByTitle(prezis, req.params.title);
+    res.send(result);
+  }
+
   return next();
 });
+
+// return prezis sorted by the date of creation
+server.get("/prezis/sortByDate",function(req, res, next){
+  res.contentType = "application/json";
+  res.send(sortPreziByDate(prezis));
+  return next();
+});
+
+//--------------------------------------------
+//  HELPER METHODS
+//--------------------------------------------
+
+function findPreziByTitle(arr, title) {
+    var out = new Array();
+    var i;
+
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i]["title"] == title){
+          out.push(arr[i]);
+        }
+    }
+
+    return out;
+}
+
+function sortPreziByDate(prezi_arr) {
+    var sorted = prezi_arr.sort(function(a,b) { 
+      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime() 
+    });
+
+    var out = new Array();
+    var i;
+    for(i = 0; i < sorted.length; i++) {
+        out.push(sorted[i]);
+    }
+
+    return out;
+}
